8a970ee3d01b71b7f7c9033e7a2c80c412935ce8
Fkaique <fkaiquesp@gmail.com>
2026-02-23 00:20:41 -0300

    initial-commit

---
+++ API/server.js
@@ -0,0 +1,54 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+const port = process.env.PORT || 3000
+app.use(express.json())
+app.use(cors())
+
+const rooms = []
+
+app.get('/rooms', (req, res) => {
+    res.send(rooms)
+})
+
+app.post('/rooms', (req, res) => {
+    const {name, pass} = req.body
+
+    if (!name || !pass) {
+        return res.status(400).send("solicitação inválida! a sala deve ter um nome e uma senha.")
+    }
+    if (name.length === 0 || pass.length === 0) {
+        return res.status(400).send("solicitação inválida! os campos devem ter pelo menos 1 caracter.")
+    }
+
+    const roomExists = rooms.find(r => r.name === name)
+
+    if (roomExists) {
+        return res.status(400).send("Já existe uma sala com esse nome.")
+    }
+
+    const newRoom = { name, pass }
+    rooms.push(newRoom)
+
+    res.status(201).send(newRoom)
+})
+
+app.delete('/rooms/:name', (req, res) => {
+    const { name } = req.params
+
+    const index = rooms.findIndex(e => e.name === name)
+
+    if (index === -1) {
+        return res.status(404).send("Sala não encontrada.")
+    }
+
+    rooms.splice(index, 1)
+    
+    res.send(rooms)
+})
+
+app.listen(port, () => {
+    console.log(`Servidor inicializado em http://localhost:${port}`);
+
+})
---
+++ front/main.js
@@ -0,0 +1,35 @@
+const criarSala = document.getElementById('criar-sala')
+const deletaSala = document.getElementById('deletar-sala')
+const criaNomeSala = document.getElementById('nameCreateRoom')
+const criaSenhaSala = document.getElementById('passCreateRoom')
+const deletaNomeSala = document.getElementById('nameDeleteRoom')
+
+criarSala.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    fetch('http://localhost:3000/rooms', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            name: criaNomeSala.value,
+            pass: criaSenhaSala.value
+        })
+    })
+        .then(res => res.json())
+        .then(data => console.log("Criado:", data))
+})
+
+deletaSala.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    fetch(`http://localhost:3000/rooms/${deletaNomeSala.value}`, { method: 'DELETE' })
+        .then(res => res.json())
+        .then(data => console.log("Deletado", data))
+})
+
+
+fetch('http://localhost:3000/rooms')
+    .then(res => res.json())
+    .then(data => console.log("Lista:", data))
